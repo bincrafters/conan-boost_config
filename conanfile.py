e4228d18db869ff7b5a134dbb8cba428e6b26628
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:45 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -10,14 +10,10 @@ class BoostConfigConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-config"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
-
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
 
     @property
     def env(self):
@@ -28,5 +24,8 @@ class BoostConfigConan(ConanFile):
         except:
             pass
         return super(self.__class__, self).env
+
+    def package_id(self):
+        self.info.header_only()
 
     # END
