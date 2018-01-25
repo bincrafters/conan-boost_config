9d092d3973f2f559b853e282c1cda0d533bca5e7
Jerry W <jerrywiltse@gmail.com>
2018-01-25 17:54:33 -0500

    switch deps to stable

--- build.py
+++ build.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bincrafters import build_template_boost_default_header_only
+from bincrafters import build_template_boost_header_only
 
 
 if __name__ == "__main__":
-    builder = build_template_boost_default_header_only.get_builder()
+    builder = build_template_boost_header_only.get_builder()
     builder.run()
 
     
