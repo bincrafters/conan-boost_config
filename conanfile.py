1383c536f342a3302b7c837b0b068ac9a5ec5eab
Rene Rivera <grafikrobot@gmail.com>
2018-04-10 09:47:09 -0500

    Initial 1.67.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,7 @@ from conans import ConanFile, tools
 
 class BoostConfigConan(ConanFile):
     name = "boost_config"
-    version = "1.66.0"
-    url = "https://github.com/bincrafters/conan-boost_config"
+    version = "1.67.0"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
     lib_short_names = ["config"]
@@ -25,15 +24,16 @@ class BoostConfigConan(ConanFile):
             self.copy(pattern="Jamfile*", dst=os.path.join(lib_short_name, "lib","checks"), src=checks_dir)
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.67.0@bincrafters/testing"
     )
-    
+
     # BEGIN
 
-    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
+    url = "https://github.com/bincrafters/conan-boost_config"
+    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.67.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
@@ -61,7 +61,5 @@ class BoostConfigConan(ConanFile):
             import boost_package_tools  # pylint: disable=F0401
             boost_package_tools.package_info(self)
         getattr(self, "package_info_additional", lambda:None)()
-
-
 
     # END
