a91a7cca57c6a3a33bcd515d6a4cc382eb001115
Jerry W <jerrywiltse@gmail.com>
2018-09-29 17:09:24 -0400

    move to python_requires strategy

--- conanfile.py
+++ conanfile.py
@@ -1,65 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import python_requires
+import os
 
 
-class BoostConfigConan(ConanFile):
+base = python_requires("boost_base/1.67.0@bincrafters/testing")
+
+class BoostConfigConan(base.BoostBaseConan):
     name = "boost_config"
-    version = "1.67.0"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    exports = ["LICENSE.md"]
+    url = "https://github.com/bincrafters/conan-boost_config"
     lib_short_names = ["config"]
-    is_header_only = True
-
-    def package_id_additional(self):
-        self.info.header_only()
+    header_only_libs = ["config"]
 
     def package_additional(self):
-        import os.path
         for lib_short_name in self.lib_short_names:
             checks_dir = os.path.join(lib_short_name, "checks")
             self.copy(pattern="*.jam", dst=os.path.join(lib_short_name, "lib","checks"), src=checks_dir)
             self.copy(pattern="*.cpp", dst=os.path.join(lib_short_name, "lib","checks"), src=checks_dir)
-            self.copy(pattern="Jamfile*", dst=os.path.join(lib_short_name, "lib","checks"), src=checks_dir)
+            self.copy(pattern="Jamfile*", dst=os.path.join(lib_short_name, "lib","checks"), src=checks_dir)  
 
-    requires = (
-        "boost_package_tools/1.67.0@bincrafters/testing"
-    )
 
-    # BEGIN
-
-    url = "https://github.com/bincrafters/conan-boost_config"
-    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
-    license = "BSL-1.0"
-    short_paths = True
-    build_requires = "boost_generator/1.67.0@bincrafters/testing"
-
-    def package_id(self):
-        getattr(self, "package_id_additional", lambda:None)()
-
-    def source(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.source(self)
-        getattr(self, "source_additional", lambda:None)()
-
-    def build(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.build(self)
-        getattr(self, "build_additional", lambda:None)()
-
-    def package(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package(self)
-        getattr(self, "package_additional", lambda:None)()
-
-    def package_info(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package_info(self)
-        getattr(self, "package_info_additional", lambda:None)()
-
-    # END
